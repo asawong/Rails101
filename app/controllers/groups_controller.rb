7073032ca56de0875fe87d5d906f234c432d9464
AsaWong <asawong0115@163.com>
2016-12-22 22:28:36 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,17 @@
 class GroupsController < ApplicationController
+
   def index
     @groups = Group.all
   end
+
   def new
    @group = Group.new
   end
+
+   def show
+     @group = Group.find(params[:id])
+   end
+
    def create
      @group = Group.new(group_params)
      @group.save
